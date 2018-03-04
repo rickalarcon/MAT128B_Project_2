a6c03d3b9acc991a1a34212056670d1231622a76
rickalarcon <35351964+rickalarcon@users.noreply.github.com>
2018-03-03 18:27:46 -0800

    generating average numbers from the train dataset and creating their plots

--- Q2_project2.m
+++ Q2_project2.m
@@ -1,33 +1,22 @@
-
-%It contains a training set of 60,000 numbers and a test set of 10,000 numbers.
 load('mnist_all.mat');
-% to see the what is in our data
-who
-%ie. each row in train 0 is one handwritten zero
-%To visulize the first image in train0, we have:
-digit= train0(1,:);
-digitImage = reshape(digit,28,28);
+%I created two for loops to create the matrix average
+% and to iterite each row to find its imagen
+
+for k = 0:9
+    %this loop created the table of 10 x 784 columns of average numbers
+    for i = 0:9
+        filename=sprintf('%s%d','train',i);
+        myVar = eval(filename);
+        T(i+1,:) = mean(myVar);
+    end
+%the second loop wil generate the imagens for each average number
+% they will be display in one plot using subplot()
+subplot(2,5,k+1);
+number=T(k+1,:);
+digitImage = reshape(number,28,28);
 image(rot90(flipud(digitImage),-1));
 colormap(gray(256)),axis square tight off;
-
-%get average look of the digits
-
-function average = myaverage(db)
-    newimage = [];
-    i=0;
-    length = length(db);
-    for i = 1:length
-        newimagen[i]= mean(data[:,i+1]);
-    end
+hold on;
 end
 
-%{
-# get average look of the digit
-get_average <- function(data){
-  pixel <- vector(mode = "numeric", length = 256)
-  for(i in 1:256){
-    pixel[i] <- mean(data[,i+1])
-  }
-  image_mat <- matrix(pixel, 16, 16)
-  image(image_mat, col = paste("grey", 1:99), ylim = c(1,0))}
-}%
+
